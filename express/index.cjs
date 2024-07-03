d22514eb992a462bdc2d7bfbb70dd10121af8c45
marc-tan <marctan13@gmail.com>
2024-07-03 14:53:23 -0700

    pull from main

--- express/index.cjs
+++ express/index.cjs
@@ -631,7 +631,7 @@ app.post('/job/add', async (req, res) => {
       !validState(state)              ||
       !validA(experienceLevel, 255)   ||
       !validSAN(employmentType, 255)  ||
-      !validN(companySize)            ||
+      !validSAN(companySize, 255)     ||
       !validN(salaryLow)              ||
       !validN(salaryHigh)             ||
       !validJSON(benefits)            ||
